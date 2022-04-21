3f0d960dea00281e1a939e2b5912945c11029901
MitchWijt <mitchel@wijt.net>
2022-04-21 10:25:46 +0200

    Initial setup of game architecture

---
+++ src/Game.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Game {
+    private ArrayList<Planet> planets;
+    private SpaceShip ship;
+
+    public void startGame() {
+
+
+    }
+
+    public void stopGame() {
+        System.out.println("Game has stopped. Here is your fly log!");
+    }
+
+    public void createPlanets() {
+
+    }
+}
